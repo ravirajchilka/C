add6f16e71ad3c640814ab4d038954f08f285a34
ravirajchilka <ravirajchilka10@gmail.com>
2023-03-13 23:12:29 -0400

    check digit or not

---
+++ cprogramsbasics/char_digit_or_not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+ char ch = '8';
+
+ if(ch>='0' && ch<='9') {
+	  	printf("Given Character %c is Digit.", ch);
+	 } else {
+	  	printf("Given Character %c is Not Digit.", ch);
+	 }
+
+  return 0;
+}   
