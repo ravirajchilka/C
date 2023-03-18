29bf25383a05a0b6b6019c7bf52541a6041ad365
ravirajchilka <ravirajchilka10@gmail.com>
2023-03-17 23:42:33 -0400

    dictionary using struct

--- cprogramsbasics/dictionary_using_Struct.c
+++ cprogramsbasics/dictionary_using_Struct.c
@@ -9,8 +9,7 @@ struct Person
 
 int main() {
     int totalRecords = 5;
-    struct Person *per = malloc(sizeof(struct Person) 
-    * totalRecords);
+    struct Person *per = malloc(sizeof(struct Person) * totalRecords);
 
     per[0].age = 8;
     per[0].name = "Sam";
@@ -24,6 +23,7 @@ int main() {
     for(int i=0;i<3;i++) {
       printf("age %d\n name %s\n", per[i].age, per[i].name);
     }
+    
     free(per);
     return 0;
 
