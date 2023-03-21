814f7fbc09c1a73b16a46b744669bd472c29188d
ravirajchilka <ravirajchilka10@gmail.com>
2023-03-21 00:43:00 -0400

    most occurred char in string using array

---
+++ cprogramsbasics/most_occured_char_using_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+  char s[] = "twitter";  
+  int ar[256] = {0};
+  int len = strlen(s);
+  int max = 0;
+  char maxCharValue;
+
+  for(int i=0;i<len;i++) {
+    ar[s[i]]++;
+  }
+
+  for(int i=0;i<len;i++) {
+     if(max< ar[s[i]]) {
+      max = ar[s[i]];
+      maxCharValue = s[i];
+     }
+  }
+
+  printf("%c, %d",maxCharValue,max);
+  return 0;
+}
