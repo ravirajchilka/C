be42b99451ead7c28f836b65384a2e40a730403d
ravirajchilka <ravirajchilka10@gmail.com>
2023-06-24 23:38:21 -0400

    stars pyramid pattern

---
+++ C_Basic_Programs/stars_pyarmid.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/*
+ 	*
+   ***
+  *****
+ *******
+*********
+
+*/
+
+int main() {
+	int rows = 5;
+	int n = rows;
+	int spaces = rows-1; 
+
+	for(int i=0;i<n;i++) {
+		int odd = (2*i)+1;
+		for(int j=spaces-i;j>0;j--) {
+			printf(" ");
+		}
+		for(int k=odd;k>0;k--) {
+			printf("*");
+		}
+		for(int j=spaces-i;j>0;j--) {
+			printf(" ");
+		}
+		printf("\n");	
+	}
+
+	return 0;	
+}
